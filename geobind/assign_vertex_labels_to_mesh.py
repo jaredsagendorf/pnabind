3f42d1feaf20cc316d1b734aa25f71e536964dab
Jared Sagendorf <jsagendorf@gmail.com>
2020-10-30 13:02:37 -0700

    moved smoothing function to geobind.mesh

--- geobind/assign_vertex_labels_to_mesh.py
+++ geobind/assign_vertex_labels_to_mesh.py
@@ -137,56 +137,6 @@ def segmentsIntersectTriangles(s, t):
     
     return np.nonzero(np.logical_not(np.sum(cross*same_volume, axis=1)))[0]
 
-def smoothLabels(mesh, key, class_label=1, threshold=16.0):
-    # Laplacian smoothing
-    #Yh1 = oneHotEncode(Y, nc)
-    #Yh2 = 2*Yh1 # count each vertex as two votes
-    #for i in range(mesh.nV):
-        #neighbors = mesh.findNeighbors(i)
-        #for n in neighbors:
-            #Yh2[i] += Yh1[n]
-    #Y = np.argmax(Yh2, axis=1)
-    
-    #### generate face labels taking majority vote of vertex labels
-    y_face = ((mesh.vertex_attributes[key][mesh.faces] == class_label).sum(axis=1) >= 2).astype(np.int32)
-    Nf = y_face.shape[0]
-    node_mask = (y_face == 1)
-    Nc = node_mask.sum()
-    
-    #### create a graph with <class> faces as vertices and connect two vertices if the faces share an edge. 
-    E_face = mesh.face_adjacency
-    edge_mask = node_mask[E_face[:,0]]*node_mask[E_face[:,1]] # edges where both nodes are a <class> face
-    
-    # create an index from 0..Nf-1 to 0..Nc-1 when we apply the node mask or edge mask
-    map_c = np.empty(Nf, dtype=np.int32)
-    map_c[node_mask] = np.arange(Nc)
-    
-    # map the <class> edges to be within range of 0..Nc-1 and make undirected
-    c_edges = map_c[E_face[edge_mask]]
-    e1, e2 = c_edges[:, 0], c_edges[:, 1]
-    e1, e2 = np.hstack((e1, e2)), np.hstack((e2, e1))
-    c_edges = np.stack((e1, e2), axis=1)
-    
-    # get <class> nodes from 0..Nc-1
-    c_nodes = map_c[node_mask]
-    
-    #### find all connected components in the <class> faces graph
-    components = trimesh.graph.connected_components(c_edges, min_len=0, nodes=c_nodes, engine='scipy')
-    
-    map_a = np.argwhere(node_mask).flatten() # index to the the original face indices
-    component_sizes = np.array([mesh.area_faces[map_a[c]].sum() for c in components])
-    
-    #### flip labels where component_size < threshold (total triangle area)
-    components_to_flip = np.argwhere(component_sizes < threshold).flatten()
-    vertices = []
-    for ci in components_to_flip:
-        face_idx = map_a[components[ci]]
-        vertices.append(mesh.faces[face_idx].flatten())
-    
-    if(len(vertices) > 0):
-        vertices = np.hstack(vertices)
-        mesh.vertex_attributes[key][vertices] = 1 - class_label
-
 def assignMeshLabelsFromStructure(structure, mesh, atom_mapper,
         distance_cutoff=4.0,
         hydrogens=True,
@@ -230,8 +180,7 @@ def assignMeshLabelsFromStructure(structure, mesh, atom_mapper,
     
     if smooth:
         # smooth labels using a smoothing scheme
-        Y = smoothLabels(mesh, Y, 0)
-        Y = smoothLabels(mesh, Y, 1)
+        pass
     
     if mask :
         # mask the boundaries of any binding region
