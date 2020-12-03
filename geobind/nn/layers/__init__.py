fb9f93ced821e42e922e629b05e1060d06fbed2e
Jared Sagendorf <jsagendorf@gmail.com>
2020-12-02 16:00:18 -0800

    removed edge_pooling_ef

--- geobind/nn/layers/__init__.py
+++ geobind/nn/layers/__init__.py
@@ -1,9 +1,11 @@
-from .edge_pooling_ef import EdgePooling_EF
+from .edge_pooling import EdgePooling
 from .nnre_loss import NNRELoss
 from .continuous_crf import ContinuousCRF
+from .mesh_pooling import MeshPooling
 
 __all__ = [
-    "EdgePooling_EF",
+    "EdgePooling",
+    "MeshPooling",
     "NNRELoss",
     "ContinuousCRF"
 ]
