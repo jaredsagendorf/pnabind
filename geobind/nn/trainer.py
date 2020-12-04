9f49c557ba41c5ae814a2783f74359902cf252fb
timkartar <timkartar7879@gmail.com>
2020-12-03 17:16:34 -0800

    everything working, pytorch only, tf not needed

--- geobind/nn/trainer.py
+++ geobind/nn/trainer.py
@@ -7,7 +7,6 @@ from os.path import join as ospj
 import torch
 from torch.optim.lr_scheduler import ReduceLROnPlateau, OneCycleLR, ExponentialLR
 from torch_geometric.nn import DataParallel
-import tensorflow as tf
 import numpy as np
 
 # geobind modules
@@ -80,34 +79,6 @@ class Trainer(object):
         # history
         self.metrics_history = {'epochs': []}
     
-    def log_histogram(self, tag, values, step, bins=1000):
-        """Logs the histogram of a list/vector of values."""
-        
-        # Create histogram using numpy        
-        counts, bin_edges = np.histogram(values, bins=bins)
-
-        # Fill fields of histogram proto
-        hist = tf.HistogramProto()
-        hist.min = float(np.min(values))
-        hist.max = float(np.max(values))
-        hist.num = int(np.prod(values.shape))
-        hist.sum = float(np.sum(values))
-        hist.sum_squares = float(np.sum(values**2))
-
-        # Requires equal number as bins, where the first goes from -DBL_MAX to bin_edges[1]
-        # See https://github.com/tensorflow/tensorflow/blob/master/tensorflow/core/framework/summary.proto#L30
-        # Thus, we drop the start of the first bin
-        bin_edges = bin_edges[1:]
-
-        # Add bin edges and counts
-        for edge in bin_edges:
-            hist.bucket_limit.append(edge)
-        for c in counts:
-            hist.bucket.append(c)
-
-        # Create and write Summary
-        summary = tf.Summary(value=[tf.Summary.Value(tag=tag, histo=hist)])
-        self.writer.add_summary(summary, step)
    
     def train(self, nepochs, dataset,
         validation_dataset=None, batch_loss_every=4, eval_every=2, debug=False,
@@ -170,15 +141,16 @@ class Trainer(object):
                     for name, param in self.model.named_parameters():
                         if (name in params_to_write) and param.requires_grad:
                             if(param.data.cpu().numpy().flatten().shape[0] == 1):
-                                    self.writer.add_scalar(name, param.data.cpu().numpy()[0], self.batch_count)
-                                    self.writer.add_scalar(name + "_grad", param.grad.cpu().numpy()[0], self.batch_count)
+                                    self.writer.add_scalar(name, param.data.cpu().numpy()[0], batch_count)
+                                    self.writer.add_scalar(name + "_grad", param.grad.cpu().numpy()[0], batch_count)
                             elif(param.data.cpu().numpy().flatten().shape[0] <= 4):
                                 for i in range(1,param.data.cpu().numpy().flatten().shape[0] + 1):
-                                    self.writer.add_scalar(name + "_" + str(i), param.data.cpu().numpy().flatten()[i-1], self.batch_count)
-                                    self.writer.add_scalar(name + "_grad_" + str(i), param.grad.cpu().numpy().flatten()[i-1], self.batch_count)
+                                    self.writer.add_scalar(name + "_" + str(i), param.data.cpu().numpy().flatten()[i-1], batch_count)
+                                    self.writer.add_scalar(name + "_grad_" + str(i), param.grad.cpu().numpy().flatten()[i-1], batch_count)
                             else:
-                                self.log_histogram(name, param.data.cpu().numpy().flatten(), self.batch_count)
-                                self.log_histogram(name + "_grad", param.grad.cpu().numpy().flatten(), self.batch_count)
+                                self.writer.add_histogram(name, param.data.cpu().numpy().flatten(), batch_count)
+                                self.writer.add_histogram(name + "grad", param.grad.cpu().numpy().flatten(), batch_count)
+                                self.writer.flush()  ## remove this line, if results in slowdown, flush only at end
 
                 # update batch count
                 batch_count += 1
