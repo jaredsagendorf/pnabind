d6c439c1700d5331032c11fd8a9137e00d8a2f8f
Jared Sagendorf <jsagendorf@gmail.com>
2020-12-15 15:26:46 -0800

    added new conv_pool model

--- geobind/nn/models/__init__.py
+++ geobind/nn/models/__init__.py
@@ -1,9 +1,11 @@
 from .conv_edge_pool import NetConvEdgePool
+from .conv_pool import NetConvPool
 from .point_net import PointNetPP
 from .multi_branch import MultiBranchNet
 
 __all__ = [
     "NetConvEdgePool",
+    "NetConvPool",
     "PointNetPP",
     "MultiBranchNet"
 ]
