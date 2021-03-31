d8c1fc8a9b8a754edf92d47edca520e4243f7ca6
Jared Sagendorf <jsagendorf@gmail.com>
2021-03-31 16:26:26 -0700

    return indices or mask

--- geobind/nn/utils/balanced_class_indices.py
+++ geobind/nn/utils/balanced_class_indices.py
@@ -1,7 +1,7 @@
 # third party modules
 import numpy as np
 
-def balancedClassIndices(y, classes, max_percentage=1.0, shuffle=True):
+def balancedClassIndices(y, classes, max_percentage=1.0, shuffle=True, return_mask=True):
     """ create a balanced index set from a vector of labels """
     idxs = []
     for c in classes:
@@ -12,10 +12,12 @@ def balancedClassIndices(y, classes, max_percentage=1.0, shuffle=True):
     for i in range(len(idxs)):
         # exclude any indices above nb
         idx = np.argwhere(idxs[i]).flatten()
-        if(shuffle):
+        if shuffle :
             np.random.shuffle(idx)
         idxs[i][idx[nb:]] = False
     
     idxb = np.array(idxs, dtype=bool).sum(axis=0, dtype=bool) # a balanced index set
-    
-    return idxb
+    if return_mask:
+        return idxb
+    else:
+        return np.argwhere(idxb).flatten()
