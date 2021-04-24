050c2619cfaec6421e403532bfcf88f603c1df3b
Jared Sagendorf <jsagendorf@gmail.com>
2021-04-23 23:32:37 -0700

    added option to push threshold higher or lower

--- geobind/nn/metrics/choose_binary_threshold.py
+++ geobind/nn/metrics/choose_binary_threshold.py
@@ -1,7 +1,14 @@
 # third party modules
 import numpy as np
 
-def chooseBinaryThreshold(y_gt, probs, metric_fn, score='metric_value', criteria='max', beta=2, n_samples=25, **kwargs):
+def chooseBinaryThreshold(y_gt, probs, metric_fn, 
+        score='metric_value',
+        criteria='max',
+        beta=2,
+        n_samples=25,
+        minimize_threshold=False,
+        **kwargs
+    ):
     """ Choose a threshold value which meets the following criteria:
         y_gt: ...
         probs: ...
@@ -21,13 +28,15 @@ def chooseBinaryThreshold(y_gt, probs, metric_fn, score='metric_value', criteria
     # choose what we are actually evaluating
     m = lambda t: metric_fn(y_gt, probs >= t, **kwargs)
     values = np.array(list(map(m, thresholds)))
-    if(score == 'f-beta'):
-        values = (1+beta**2)*(thresholds*values)/((beta**2)*thresholds + values)
+    if score == 'f-beta':
+        if minimize_threshold:
+            t = 1 - thresholds
+        values = (1+beta**2)*(t*values)/((beta**2)*t + values)
     
     # choose how to evaluate
-    if(criteria == 'max'):
+    if criteria == 'max':
         idx = np.argmax(values)
-    elif(criteria == 'min'):
+    elif criteria == 'min':
         idx = np.argmin(values)
     
     return thresholds[idx], values[idx]
