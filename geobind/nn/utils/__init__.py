efe55c0148edd42c145ab3e4330c8882357056a0
Jared <jsagendorf@gmail.com>
2022-01-07 14:52:23 -0800

    added new utils

--- geobind/nn/utils/__init__.py
+++ geobind/nn/utils/__init__.py
@@ -3,12 +3,16 @@ from .class_weights import classWeights
 from .load_data import ClassificationDatasetMemory
 from .load_data import loadDataset
 from .mlp import MLP
+from .load_module import loadModule
+from .add_weight_decay import addWeightDecay
 
 __all__ = [
     "balancedClassIndices",
     "classWeights",
     "ClassificationDatasetMemory",
     "loadDataset",
+    "loadModule",
+    "addWeightDecay",
     "MLP"
 ]
 
