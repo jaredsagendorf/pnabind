77e14139ea6befc9147c6eeb4d85fc5b176a8397
Jared Sagendorf <jsagendorf@gmail.com>
2022-04-22 16:46:34 -0700

    added local attention pooling

--- geobind/nn/layers/__init__.py
+++ geobind/nn/layers/__init__.py
@@ -3,11 +3,13 @@ from .nnre_loss import NNRELoss
 from .continuous_crf import ContinuousCRF
 from .mesh_pooling import MeshPooling
 from .hist_conv import PPHConv
+from .local_attn_pooling import LocalAttentionPooling
 
 __all__ = [
     "EdgePooling",
     "MeshPooling",
     "NNRELoss",
     "ContinuousCRF",
-    "PPHConv"
+    "PPHConv",
+    "LocalAttentionPooling"
 ]
