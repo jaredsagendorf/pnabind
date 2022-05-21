f1e33aae194d83043f6b77758d52418e73bffcef
Jared Sagendorf <jsagendorf@gmail.com>
2022-05-20 18:05:17 -0700

    avoid NaN values

--- geobind/utils/clip_outliers.py
+++ geobind/utils/clip_outliers.py
@@ -8,12 +8,12 @@ def clipOutliers(data, method="IQR", axis=None):
         except ModuleNotFoundError:
             raise ModuleNotFoundError("The dependency 'SciPy' is required for this functionality!'")
         Z = np.abs(zscore(data, axis=axis))
-        mask = (Z > 3)
+        mask = np.logical_or((Z > 3), np.isnan(data))
     elif method == "IQR":
-        Q1 = np.quantile(data, 0.25, axis=axis)
-        Q3 = np.quantile(data, 0.75, axis=axis)
+        Q1 = np.nanquantile(data, 0.25, axis=axis)
+        Q3 = np.nanquantile(data, 0.75, axis=axis)
         IQR = Q3 - Q1 
-        mask = np.logical_or((data < (Q1-1.5*IQR)), (data > (Q3+1.5*IQR)))
+        mask = np.logical_or((data < (Q1-1.5*IQR)), (data > (Q3+1.5*IQR)), np.isnan(data))
     mdata = np.ma.array(data, mask=mask)
     lower = mdata.min(axis=axis)
     upper = mdata.max(axis=axis)
