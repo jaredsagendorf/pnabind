0d13b8f59ee594c085aa412dea7a1904c7208f97
Jared Sagendorf <jsagendorf@gmail.com>
2022-06-22 14:59:49 -0700

    robust to NaN values

--- geobind/utils/clip_outliers.py
+++ geobind/utils/clip_outliers.py
@@ -1,21 +1,32 @@
 # third party packages
 import numpy as np
 
-def clipOutliers(data, method="IQR", axis=None):
+def clipOutliers(data, method="IQR", axis=None, impute_nans=True):
     if method == "z-score":
         try:
             from scipy.stats import zscore
         except ModuleNotFoundError:
             raise ModuleNotFoundError("The dependency 'SciPy' is required for this functionality!'")
         Z = np.abs(zscore(data, axis=axis))
-        mask = np.logical_or((Z > 3), np.isnan(data))
+        mask = (Z > 3) + np.isnan(data)
     elif method == "IQR":
         Q1 = np.nanquantile(data, 0.25, axis=axis)
         Q3 = np.nanquantile(data, 0.75, axis=axis)
-        IQR = Q3 - Q1 
-        mask = np.logical_or((data < (Q1-1.5*IQR)), (data > (Q3+1.5*IQR)), np.isnan(data))
+        IQR = Q3 - Q1
+        mask = (data < (Q1-1.5*IQR)) + (data > (Q3+1.5*IQR)) + np.isnan(data)
     mdata = np.ma.array(data, mask=mask)
-    lower = mdata.min(axis=axis)
-    upper = mdata.max(axis=axis)
+    lower = np.ma.getdata(mdata.min(axis=axis))
+    upper = np.ma.getdata(mdata.max(axis=axis))
     
-    return np.clip(data, lower, upper)
+    data = np.clip(data, lower, upper)
+    
+    if impute_nans:
+        inds = np.where(np.isnan(data))
+        mean = np.nanmean(data, axis=axis)
+        
+        if data.ndim == 1:
+            data[inds] = mean
+        else:
+            data[inds] = np.take(mean, inds[1])
+    
+    return data
