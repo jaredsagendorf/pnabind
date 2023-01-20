e0c8de0241929c03e2b8d1bb90e2447675d1396f
Jared Sagendorf <jsagendorf@gmail.com>
2023-01-20 14:27:53 -0800

    add residue label smoothing

--- geobind/vertex_labels_to_residue_labels.py
+++ geobind/vertex_labels_to_residue_labels.py
@@ -7,67 +7,62 @@ from geobind.structure import getAtomKDTree
 from geobind.structure.data import data
 from geobind.structure import mapPointFeaturesToStructure, getResidueDistance
 
-def smoothResiduePredictions(structure, residue_dict, nc=2, edge_dist_threshold=4.0, niter=1, hydrogens=False, ignore_class=set()):
+def smoothResidueLabels(residues, labels, nc=2, distance_cutoff=4.0, iterations=1, skip_hydrogens=True, ignore_class=set()):
     """Apply laplacian smoothing"""
     try:
         import networkx as nx
     except ModuleNotFoundError:
         raise ModuleNotFoundError("The dependency 'networkx' is required for this functionality!")
     
-    residues = []
     atoms = []
-    labels = []
-    count = 0
-    for rid in residue_dict:
-        res = structure.get_residue(rid[3], rid[2])
-        labels.append(residue_dict[rid]["label"])
-        for atom in res:
-            if (not hydrogens) and atom.element == 'H':
+    for residue in residues:
+        for atom in residue:
+            if skip_hydrogens and atom.element == 'H':
                 continue
             atoms.append(atom)
-        residue_dict[rid]["node"] = count
-        residues.append(rid)
-        count += 1
-    
     kdt = getAtomKDTree(atoms, engine='biopython')
     
     # get connected components
     G = nx.Graph()
-    labels = np.array(labels, dtype=int)
-    for i in range(count):
+    for i in range(len(residues)):
         G.add_edge(i, i) # self loop to count self
-
-    neighbors = kdt.search_all(edge_dist_threshold, level='R')
+    residue_node = {residue: i for i, residue in enumerate(residues)}
+    
+    neighbors = kdt.search_all(distance_cutoff, level='R')
     for pair in neighbors:
         G.add_edge(
-            residue_dict[pair[0].get_full_id()]["node"],
-            residue_dict[pair[1].get_full_id()]["node"]
+            residue_node[pair[0]],
+            residue_node[pair[1]]
         )
     
     # smooth labels with simple majority vote
-    for _ in range(niter):
-        new_labels = np.zeros(count)
-        for i in range(count):
+    for _ in range(iterations):
+        new_labels = labels.copy()
+        for i in range(len(labels)):
             if labels[i] in ignore_class:
                 continue
             ni = list(G.neighbors(i))
-            new_labels[i] = np.bincount(labels[ni]).argmax()
+            counts = np.bincount(labels[ni], minlength=nc)
+            if counts[0] == counts[1]:
+                # tie - leave class alone
+                continue
+            new_labels[i] = counts.argmax()
         labels = new_labels
     
-    # update labels in residue_dict
-    for i in range(count):
-        residue_dict[residues[i]]["label"] = int(new_labels[i])
+    return labels
 
-def vertexLabelsToResidueLabels(structure, mesh, Y, 
-            nc=2,
-            kdt=None,
-            id_format='dnaprodb',
-            null_class=0,
-            return_kdt=False,
-            thresholds=None,
-            smooth_labels=False,
-            **kwargs
-    ):    
+def vertexLabelsToResidueLabels(structure, vertices, Y,
+        areas=None,
+        nc=2,
+        id_format='biopython',
+        include_buried=False,
+        kdt=None,
+        return_kdt=False,
+        residue_dict=None,
+        store_residue=False,
+        separate_backbone=False,
+        **kwargs
+    ):
     if isinstance(structure, list):
         atoms = structure # we were given a list of atoms
     else:
@@ -76,39 +71,38 @@ def vertexLabelsToResidueLabels(structure, mesh, Y,
     if kdt is None:
         kdt = getAtomKDTree(atoms)
     
-    if thresholds is None:
-        thresholds = data.sesa_cutoffs # these should be used for training labels only
-    elif isinstance(thresholds, float):
-        _t = {}
-        for res in data.standard_residues:
-            _t[res] = thresholds
-        thresholds = _t
+    if areas is None:
+        areas = np.ones_like(Y)
     
-    # get vertex areas
-    areas = np.zeros_like(Y, dtype=np.float32)
-    np.add.at(areas, mesh.faces[:, 0], mesh.area_faces/3)
-    np.add.at(areas, mesh.faces[:, 1], mesh.area_faces/3)
-    np.add.at(areas, mesh.faces[:, 2], mesh.area_faces/3)
-    
+    # assign vertex areas to class areas
     for c in range(nc):
         mask = (Y == c)
         # map every vertex area to nearest atom, based on class of vertex
-        mapPointFeaturesToStructure(mesh.vertices, atoms, areas*mask, 'area_{}'.format(c), kdtree=kdt)
+        mapPointFeaturesToStructure(vertices, atoms, areas*mask, 'area_{}'.format(c), kdtree=kdt, impute=True)
     
     # aggregate over atom areas
-    residue_dict = {}
+    if residue_dict is None:
+        residue_dict = {}
+    bb_atoms = set(['N', 'C', 'O', 'OXT'])
     for atom in atoms:
         areas = np.zeros(nc)
         for c in range(nc):
             key = 'area_{}'.format(c)
             if key in atom.xtra:
                 areas[c] += atom.xtra[key]
-        if areas.sum() == 0:
+        
+        if (not include_buried) and areas.sum() == 0:
             # atom makes no contribution, skip
             continue
         
         residue = atom.get_parent()
-        residue_id = residue.get_full_id()
+        if id_format == "biopython":
+            residue_id = residue.get_full_id()
+        elif id_format == "dnaprodb":
+            residue_id = residue.get_full_id()
+            residue_id = '{}.{}.{}'.format(residue_id[2], residue_id[3][1], residue_id[3][2])
+        else:
+            residue_id = residue
         
         if residue_id not in residue_dict:
             # make a new residue entry
@@ -116,33 +110,15 @@ def vertexLabelsToResidueLabels(structure, mesh, Y,
                 'residue_name': residue.get_resname(),
                 'class_areas': np.zeros(nc)
             }
-        
-        residue_dict[residue_id]['class_areas'] += areas
-    
-    # determine residue class
-    ni = null_class
-    for residue_id in residue_dict:
-        residue_dict[residue_id]['class_areas'][ni] = 0 # mask out the null class area
-        ci = np.argmax(residue_dict[residue_id]['class_areas'])
-        resn = residue_dict[residue_id]['residue_name']
-        
-        if residue_dict[residue_id]['class_areas'][ci] > thresholds[resn]:
-            residue_dict[residue_id]['label'] = ci
+            if store_residue:
+                residue_dict[residue_id]["residue"] = residue
+            if separate_backbone:
+                residue_dict[residue_id]["backbone_class_areas"] = np.zeros(nc)
+        if separate_backbone and atom.name.strip() in bb_atoms:
+            residue_dict[residue_id]['backbone_class_areas'] += areas
         else:
-            residue_dict[residue_id]['label'] = null_class
-    
-    # perform smoothing
-    if smooth_labels:
-        smoothResiduePredictions(structure, residue_dict, nc=nc, **kwargs)
-    
-    # check id format
-    if id_format == 'dnaprodb':
-        _rdict = {}
-        for residue_id in residue_dict:
-            _rid = '{}.{}.{}'.format(residue_id[2], residue_id[3][1], residue_id[3][2])
-            _rdict[_rid] = residue_dict[residue_id]
-        residue_dict = _rdict
-    
+            residue_dict[residue_id]['class_areas'] += areas
+        
     if return_kdt:
         return residue_dict, kdt
     else:
